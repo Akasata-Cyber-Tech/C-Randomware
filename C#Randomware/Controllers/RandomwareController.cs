89c13618bc3162169b2ab588685e1cde36227c4f
Admin <Admin@DESKTOP-JNA1K5D>
2023-05-31 14:43:04 +0700

    Add project files.

---
+++ C#Randomware/Controllers/RandomwareController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C_Randomware.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomwareController : ControllerBase
+    {
+        // POST: RandomwareController/Create
+        [Authorize]
+        [HttpPost("Add")]
+        public string Create(IFormCollection collection)
+        {
+            return "";
+        }
+
+        // PUT: RandomwareController/Edit/5
+        [HttpPut]
+        public string Edit(int id, IFormCollection collection)
+        {
+            return "";
+        }
+
+        // POST: RandomwareController/Delete/5
+        [HttpDelete]
+        public string Delete(int id, IFormCollection collection)
+        {
+            return "";
+        }
+    }
+}
